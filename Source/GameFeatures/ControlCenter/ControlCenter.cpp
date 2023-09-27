69dcc8708eb2bb2698bf9f666448f584f8693205
Reza Shokrie <137177631+Rezarsa82@users.noreply.github.com>
2023-09-27 10:00:38 +0330

    Update ControlCenter.cpp

--- Source/GameFeatures/ControlCenter/ControlCenter.cpp
+++ Source/GameFeatures/ControlCenter/ControlCenter.cpp
@@ -57,7 +57,7 @@ std::string ControlCenter::Tap(const std::string &attacker, const std::string &a
     throw Exception("attacked is dead");
   if (!attacker_player->HasWeapon(type))
     throw Exception("no such gun");
-  if (!(terrorist_->IsTeammate(attacker) ^ terrorist_->IsTeammate(attacked)))
+  if (!(terrorist_->IsTeammate(attacker) && terrorist_->IsTeammate(attacked)))
     throw Exception("friendly fire");
   if (attacked_player->CanAttacked(attacker_player->GetWeapon(type)->GetPower())) { attacker_player->AddKill(type); }
   return "nice shot";
