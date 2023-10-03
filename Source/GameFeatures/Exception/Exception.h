b7836e8c9372ca88f0e42c688c7c61e1a03aaa69
Reza Shokrie <137177631+Rezarsa82@users.noreply.github.com>
2023-10-03 11:33:53 +0330

    Update Exception.h

--- Source/GameFeatures/Exception/Exception.h
+++ Source/GameFeatures/Exception/Exception.h
@@ -13,4 +13,16 @@ class Exception : public std::exception {
   std::string message_;
 };
 
+ class TapException : public std::exception{
+ public:
+  explicit TapException() = default;
+  static void AttackerIsDead();
+  static void AttackedIsDead();
+  static void NoGun();
+  static void FriendlyFire();
+  private:
+  std::string message_;
+};
+
+
 #endif //COUNTER_STRIKE_GAMEFEATURES_EXCEPTION_EXCEPTION_H_
